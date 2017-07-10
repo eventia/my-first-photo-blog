2369beec0ff82ecd8e34fe3954e942caae6fc158
eventia@gmail.com <eventia@gmail.com>
2017-07-10 19:01:10 +0900

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/img', null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super(Post, self).delete(*args, **kwargs)
